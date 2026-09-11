2b922b8fe1731eda855a8b85be3e45f16fb89fcf
Sommerelad <sommerelad@gmail.com>
2026-09-11 16:04:44 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service worker for "המתכונים שלי" - enables offline use once installed.
-const CACHE_NAME = "recipes-app-cache-v7";
+const CACHE_NAME = "recipes-app-cache-v8";
 const APP_SHELL = [
   "./",
   "./index.html",
